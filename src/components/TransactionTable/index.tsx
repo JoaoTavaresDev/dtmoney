f04b5f1a50d6dc22b0d42338ed160bc00e176fb2
João <jpcovinhas0@gmail.com.br>
2022-05-30 12:23:00 -0300

    feat:Modal e Form added

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { Container } from './styles';
 
-export function TransactionTable() {
-	useEffect(() => {
-		api.get('/transactions').then(response => console.log(response.data));
-	}, []);
+interface Transaction {
+	id: string;
+	title: string;
+	amount: number;
+	type: 'income' | 'outcome';
+	category: string;
+	createdAt: string;
+}
 
-	const [transactions, setTrnssactions] = useState([]);
+export function TransactionTable() {
+	const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+	useEffect(() => {
+		api
+			.get('/transactions')
+			.then(response => setTransactions(response.data.transactions));
+	}, []);
 
 	return (
 		<Container>
@@ -22,18 +33,23 @@ export function TransactionTable() {
 				</thead>
 
 				<tbody>
-					<tr>
-						<td>Desenvolvimento de Website</td>
-						<td className="deposit">R$12.000</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2022</td>
-					</tr>
-					<tr>
-						<td>Aluguel</td>
-						<td className="withdraw">- R$1.100</td>
-						<td>Casa</td>
-						<td>17/02/2022</td>
-					</tr>
+					{transactions.map(transaction => (
+						<tr key={transaction.id}>
+							<td>{transaction.title}</td>
+							<td className={transaction.type}>
+								{new Intl.NumberFormat('pt-BR', {
+									style: 'currency',
+									currency: 'BRL',
+								}).format(transaction.amount)}
+							</td>
+							<td>{transaction.category}</td>
+							<td>
+								{new Intl.DateTimeFormat('pt-BR').format(
+									new Date(transaction.createdAt),
+								)}
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</Container>
